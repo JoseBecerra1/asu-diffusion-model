74cb40a017cce6afc900c94da82d9a63dbea01c0
JoseBecerra1 <jabecer3@asu.edu>
2025-05-07 22:23:28 +0000

    Clear unnesessary text

--- diffmodel.py
+++ diffmodel.py
@@ -2,31 +2,12 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
-# Start by setting two model parmaters, the diffusivity and the size of the model domain 
-
-# In[ ]:
-
-
 D = 100
 Lx = 300
-
-
-# Next, set up the model grid using a NumPy array
-
-# In[ ]:
-
 
 dx = 0.5
 x = np.arange(start=0,stop=Lx, step=dx)
 nx = len(x)
-
-
-# Intial conditions, inital shape of the ice cream cake
-# The cake 'C' is a step function with a high value on the left and a low value on thr right, and a step at the center of the domain
-
-# In[ ]:
-
 
 C = np.zeros_like(x)
 C_left = 500
@@ -34,44 +15,18 @@ C_right = 0
 C[x<=Lx / 2] = C_left
 C[x> Lx / 2] = C_right
 
-
-# Plot the inital profile
-
-# In[ ]:
-
-
 plt.figure()
 plt.plot(x, C, "r")
 plt.xlabel("x")
 plt.ylabel("C")
 plt.title("Initial Profile")
 
-
-# Set the number of timesteps in the model
-# Calculare a stable timestep using a stability criterion
-
-# In[ ]:
-
-
 nt = 5000
 dt = 0.5*dx**2/D
 
 
-# Loop over the time steps of the model solving the diffusion equation using the FTCS scheme shown above
-# Note the use of array operations on the variable 'C'. 
-# The boundary condtions remain fixed at each time step
-
-# In[ ]:
-
-
 for t in range(0, nt):
 	C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
-# C[1:-1] ensures first and lat vlaue (boudnary conditions) are nor touched
-# += takes the quantity (D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:]) and ads the quntitify evertime to the loop
-
-
-# In[ ]:
-
 
 plt.figure()
 plt.plot(x, C, "b")
