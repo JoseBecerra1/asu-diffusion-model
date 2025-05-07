2fead5dc961b5d301a741dbba48ad096b9181ace
JoseBecerra1 <jabecer3@asu.edu>
2025-05-07 23:30:54 +0000

    Add plot_profile function

--- diffmodel.py
+++ diffmodel.py
@@ -3,6 +3,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+def plot_profile(x, cake, color="r")
+    """Display Cake Profile."""
+    plt.figure()
+    plt.plot(x, cake, color)
+    plt.xlabel("x")
+    plt.ylabel("cake")
+    plt.title("Cake Profile")
+    
+
 
 D = 100
 Lx = 300
@@ -17,11 +26,7 @@ C_right = 0
 C[x<=Lx / 2] = C_left
 C[x> Lx / 2] = C_right
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial Profile")
+plot_profile(x,C)
 
 nt = 5000
 dt = 0.5*dx**2/D
@@ -30,9 +35,5 @@ dt = 0.5*dx**2/D
 for t in range(0, nt):
 	C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
 
-plt.figure()
-plt.plot(x, C, "b")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Final Profile")
+plot_profile(x,C, color="b")
 
